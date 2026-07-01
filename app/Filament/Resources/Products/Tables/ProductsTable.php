4ef0a704b86ae68b6f599dc5b02efe90379ff20f
unknown <mohamedelmasry1992001@gmail.com>
2026-07-01 12:03:04 +0300

    add delete action in each row

--- app/Filament/Resources/Products/Tables/ProductsTable.php
+++ app/Filament/Resources/Products/Tables/ProductsTable.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Products\Tables;
 
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Tables\Columns\TextColumn;
@@ -22,6 +23,7 @@ class ProductsTable
             ])
             ->recordActions([
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
