4839f65b953e94799bdcb8369f5a6dbfcf0fcf70
unknown <mohamedelmasry1992001@gmail.com>
2026-07-02 11:45:10 +0300

    add filters to filament table

--- app/Filament/Resources/Products/Tables/ProductsTable.php
+++ app/Filament/Resources/Products/Tables/ProductsTable.php
@@ -2,12 +2,18 @@
 
 namespace App\Filament\Resources\Products\Tables;
 
+use App\Enums\ProductStatusEnum;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Forms\Components\DatePicker;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Enums\FiltersLayout;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
 
 class ProductsTable
 {
@@ -26,8 +32,31 @@ class ProductsTable
 
             ])->defaultSort('name', 'asc')
             ->filters([
-                //
-            ])
+                // Filter::make('in stock')->query(fn(Builder $query): Builder => $query->where('status', ProductStatusEnum::In_Stock)),
+                SelectFilter::make('status')
+    ->label('Status')
+    ->options(ProductStatusEnum::class),
+                SelectFilter::make('category')->relationship('category', 'name'),
+                filter::make('created_from')->schema([
+                    DatePicker::make('created_from'),
+                ])->query(function (Builder $query, array $data): Builder {
+                    return $query
+                        ->when(
+                            $data['created_from'],
+                            fn (Builder $query, $date): Builder => $query->whereDate('created_at', '>=', $date),
+                        );
+
+                }),
+                    filter::make('created_untill')->schema([
+                    DatePicker::make('created_untill'),
+                ])->query(function (Builder $query, array $data): Builder {
+                    return $query
+                        ->when(
+                            $data['created_untill'],
+                            fn (Builder $query, $date): Builder => $query->whereDate('created_at', '<=', $date),
+                        );
+                })
+            ],layout: FiltersLayout::AboveContent)
             ->recordActions([
                 EditAction::make(),
                 DeleteAction::make(),
